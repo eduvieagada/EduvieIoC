ddb1bec251568090fcf41dd5989451c22cc114a6
Eduvie Agada <eduvieagada@gmail.com>
2019-02-16 11:54:14 +0100

    initial commit

---
+++ ConsoleApp1.Tests/ContainerTests.cs
@@ -0,0 +1,132 @@
+﻿using EduvieIoC;
+using NUnit.Framework;
+
+namespace EduvieContainer.Tests
+{
+    public class ContainerTestBase
+    {
+        protected Container Container;
+
+        [SetUp]
+        public void BeforeEach()
+        {
+            Container = new Container();
+        }
+
+        [TearDown]
+        public void AfterEach()
+        {
+            Container = null;
+        }
+    }
+
+    [TestFixture]
+    public class Container_GetInstance : ContainerTestBase
+    {
+        [Test]
+        public void CreatesAnInstanceWithNoParams()
+        {
+            var subject = (A) Container.GetInstance(typeof(A));
+
+            Assert.IsInstanceOf<A>(subject);
+        }
+
+        [Test]
+        public void CreateAnInstanceWithParams()
+        {
+            var subject = (B)Container.GetInstance(typeof(B));
+            Assert.IsInstanceOf<A>(subject.A);
+        }
+
+        [Test]
+        public void ItAllowsParameterlessConstructor()
+        {
+            var subject = (C)Container.GetInstance(typeof(C));
+            Assert.IsTrue(subject.Invoked);
+        }
+
+        [Test]
+        public void ItAllowsGenericInitialization()
+        {
+            var subject = Container.GetInstance<A>();
+            Assert.IsInstanceOf<A>(subject);
+        }
+
+    }
+
+    [TestFixture]
+    public class Container_Register : ContainerTestBase
+    {
+        [Test]
+        public void RegisterATypeFromAnInterface()
+        {
+            Container.Register<IMaterial, Plastic>();
+            var subject = Container.GetInstance<IMaterial>();
+
+            Assert.IsInstanceOf<Plastic>(subject);
+        }
+
+        interface IMaterial
+        {
+            int Weight { get; }
+        }
+
+        class Plastic : IMaterial
+        {
+            public int Weight => 42;
+        }
+
+        class Metal : IMaterial
+        {
+            public int Weight => 84;
+        }
+    }
+
+    [TestFixture]
+    public class Container_RegisterSingleton : ContainerTestBase
+    {
+        [Test]
+        public void ItReturnsSingleInstance()
+        {
+            var pet = new Pet();
+            Container.RegisterSingleton(pet);
+            var subject = Container.GetInstance<Pet>();
+
+            Assert.IsTrue(pet.Equals(subject));
+        }
+    }
+
+    internal class Pet
+    {
+        public Pet()
+        {
+        }
+    }
+
+    internal class C
+    {
+        public bool? Invoked { get; set; }
+        public C()
+        {
+            Invoked = true;
+        }
+    }
+
+    class A
+    {
+
+    }
+
+    class B
+    {
+        public B()
+        {
+
+        }
+        public A A { get; }
+        public B(A a)
+        {
+            A = a;
+        }
+    }
+}
---
+++ ConsoleApp1.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ConsoleApp1.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp1.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7ea7e605-4270-4a86-b0b9-c23b36a6206d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
